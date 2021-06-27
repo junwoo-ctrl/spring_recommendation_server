2ca035b6040c30eaaac6e9a0fcd0c5707f155a84
mrx-junwoo <rlagud67@gmail.com>
2021-06-27 09:30:14 +0900

    initialize project, 1. maven build 2. spring boot starter package .

---
+++ src/main/java/RestRecommend/RestRecommendApplication.java
@@ -0,0 +1,13 @@
+package RestRecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RestRecommend/RestRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package RestRecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
