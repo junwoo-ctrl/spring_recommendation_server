14b6e12ece841fe07641c305d1b4be6436e80c7f
mrx-junwoo <rlagud67@gmail.com>
2021-06-27 11:44:48 +0900

    bean context scanning.

--- src/main/java/RestRecommend/RestRecommendApplication.java
+++ src/main/java/RestRecommend/RestRecommendApplication.java
@@ -2,8 +2,11 @@ package RestRecommend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(packages="RestRecommend")
 public class RestRecommendApplication {
 
 	public static void main(String[] args) {
