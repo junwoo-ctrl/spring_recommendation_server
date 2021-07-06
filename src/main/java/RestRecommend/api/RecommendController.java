e0d17903bdffafe3136979984d06e72d3ff607f7
mrx-junwoo <rlagud67@gmail.com>
2021-07-06 09:59:17 +0900

    spring restapi scheme.

--- src/main/java/RestRecommend/api/RecommendController.java
+++ src/main/java/RestRecommend/api/RecommendController.java
@@ -4,22 +4,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import RestRecommend.data.RequestedWord;
-import RestRecommend.data.RecommendedWord
+import RestRecommend.data.RecommendedWord;
 import RestRecommend.v1.Recommender;
 
 
 @RestController
-@ReqeustMapping(value="/recommend")
+@RequestMapping(value="/recommend")
 public class RecommendController {
 	
-	@RequestMapping(value="/{requested_word}", method=RequestMethod.GET)
+	@RequestMapping(value="/", method=RequestMethod.POST)
 	@ResponseBody()
-	public RecommendedWord getRecommendedWord(@PathVariable("requested_word") final RequestedWord requested_word) {
+	public RecommendedWord getRecommendedWord(@RequestBody RequestedWord requested_word) throws Exception {
 		Recommender recommender = new Recommender();
-		RecommendedWord recommended_word = recommender.search(requested_word);
+		RecommendedWord recommended_word = recommender.searchRecommendWord(requested_word);
 		return recommended_word;
 	}
 
