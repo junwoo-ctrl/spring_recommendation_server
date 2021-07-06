f5a761c2bcd041a0e1264691eafe690836a38bad
mrx-junwoo <rlagud67@gmail.com>
2021-07-06 14:22:55 +0900

    define manual text obj.

---
+++ src/main/java/RestRecommend/data/ManualV1.java
@@ -0,0 +1,10 @@
+package RestRecommend.data;
+
+public class ManualV1 {
+	public String manual = "this is a manual v1.";
+
+	public ManualV1() {}
+	public String getManual() {
+		return this.manual;
+	}
+}
---
+++ src/main/java/RestRecommend/data/ManualV2.java
@@ -0,0 +1,10 @@
+package RestRecommend.data;
+
+public class ManualV2 {
+	public String manual = "this is a manual v2. this manual has more string than before.";
+
+	public ManualV2() {}
+	public String getManual() {
+		return this.manual;
+	}
+}
